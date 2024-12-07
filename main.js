59ba835506e18cb35344fb9d614f4b22db08be2c
CarlosDias <diasdev106@gmail.com>
2024-12-07 20:15:37 +0000

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+// Animação no texto
+document.addEventListener('DOMContentLoaded', () => {
+    const animatedText = document.querySelector('.animated-text');
+    animatedText.classList.add('fade-in');
+});
+
